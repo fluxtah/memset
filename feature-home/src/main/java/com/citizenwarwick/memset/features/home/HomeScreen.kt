f67183ae4037a5f5bd93c24bcc2d707801dca312
Ian Warwick <ian.warwick@just-eat.com>
2019-12-30 14:22:22 +0000

    added bottom nav to home

--- feature-home/src/main/java/com/citizenwarwick/memset/features/home/HomeScreen.kt
+++ feature-home/src/main/java/com/citizenwarwick/memset/features/home/HomeScreen.kt
@@ -1,22 +1,32 @@
 package com.citizenwarwick.memset.features.home
 
 import androidx.compose.Composable
-import androidx.ui.core.Text
 import androidx.ui.layout.Column
+import androidx.ui.layout.Container
+import androidx.ui.layout.FlexColumn
 import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
 import com.citizenwarwick.memset.router.Composer
 import com.citizenwarwick.memset.router.Router
+import com.citizenwarwick.ui.MemsetBottomNavigation
 
 class HomeScreen(private val router: Router) : Composer() {
     @Composable
     override fun compose() {
         MaterialTheme {
-            Column {
-                Text(text = "Home Screen")
-                Button(text = "Card Editor", onClick = {
-                    router.navigate("http://memset.com/cardeditor")
-                })
+            FlexColumn {
+                expanded(1f) {
+                    Container {
+                        Column {
+                            Button(text = "Go to Card Editor", onClick = {
+                                router.navigate("http://memset.com/cardeditor")
+                            })
+                        }
+                    }
+                }
+                inflexible {
+                    MemsetBottomNavigation(router)
+                }
             }
         }
     }
